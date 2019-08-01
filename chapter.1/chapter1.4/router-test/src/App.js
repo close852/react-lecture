fabeda273b1e0411b2ba953ec419bd41f7b47e37
close852 <close852@naver.com>
2019-08-01 22:41:38 +0900

    pushState(history push), replaceState(replace), onpopstate(스택에서 꺼냄)

---
+++ chapter.1/chapter1.4/router-test/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Link } from 'react-router-dom'
+import Rooms from './Rooms'
+class App extends Component {
+  render() {
+    console.log('main....')
+    return (
+      <BrowserRouter>
+        <div style={{ padding: 20, border: '5px solid gray' }}>
+          <br />
+          <Link to="/photo">사진</Link>
+          <br />
+          <Link to="/rooms">방 소개</Link>
+          <br />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/photo" component={Photo} />
+          <Route path="/rooms" component={Rooms} />
+        </div>
+      </BrowserRouter>
+    );
+  }
+}
+function Home({ match }) {
+  return <h2>이 곳은 집입니다.</h2>;
+}
+
+function Photo({ match }) {
+  return <h2>여기서 사진을 감상하세요.</h2>
+}
+
+export default App;
